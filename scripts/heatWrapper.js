e0304b008f1377d9c9cb500dc0e52dc60fbd31b6
younggam <nainjoo2001@gmail.com>
2020-05-15 20:16:13 +0900

    nothing

--- scripts/heatWrapper.js
+++ scripts/heatWrapper.js
@@ -1,4 +1,5 @@
 const heatLib=require("heatLibrary");
+//객체 깊은 복사
 function cloneObject(obj){
   var clone={};
   for(var i in obj){
@@ -9,10 +10,12 @@ function cloneObject(obj){
 }
 module.exports={
   heatGiver(Type,Entity,name,def,customEnt){
+    //상속
     const block=Object.create(heatLib.giver);
+    //객체 합치기
     Object.assign(block,def);
     const heatBlock=extendContent(Type,name,block);
-    heatBlock.entityType=prov(()=>extend(Entity,Object.assign(cloneObject(heatLib.heatProps),customEnt)));
+    heatBlock.entityType=prov(()=>extend(Entity,/*객체합치기*/Object.assign(cloneObject(heatLib.heatProps),customEnt)));
     return heatBlock;
   },
   heatUser(Type,Entity,name,def,customEnt){
